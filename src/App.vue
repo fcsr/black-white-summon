58774133539855653ad24e72f96919a89bda9e40
fcsr <gsfcsr@gmail.com>
2026-07-09 10:01:12 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-shell">
+    <TopBar />
+
+    <main class="app-main">
+      <router-view />
+    </main>
+
+    <NavBar />
+  </div>
+</template>
+
+<script setup>
+import TopBar from './components/layout/TopBar.vue'
+import NavBar from './components/layout/NavBar.vue'
+</script>
