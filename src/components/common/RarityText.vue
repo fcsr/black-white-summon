00e00fe3e4a6099ccff9ff9b26a93834931e64f8
fcsr <gsfcsr@gmail.com>
2026-07-09 10:59:30 +0800

    Create RarityText.vue

---
+++ src/components/common/RarityText.vue
@@ -0,0 +1,29 @@
+<template>
+  <span :class="rarityClass">
+    [{{ rarityLabel }}] {{ name }}
+  </span>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { RARITY_LABELS, RARITY_CLASS_MAP } from '../../utils/constants'
+
+const props = defineProps({
+  rarity: {
+    type: String,
+    default: 'normal'
+  },
+  name: {
+    type: String,
+    default: ''
+  }
+})
+
+const rarityLabel = computed(() => {
+  return RARITY_LABELS[props.rarity] || '未知'
+})
+
+const rarityClass = computed(() => {
+  return RARITY_CLASS_MAP[props.rarity] || ''
+})
+</script>
