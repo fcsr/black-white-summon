0db3f7341061833ca661e820bda7fb88058fea75
fcsr <gsfcsr@gmail.com>
2026-07-09 10:53:33 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,14 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import TopBar from './components/layout/TopBar.vue'
 import NavBar from './components/layout/NavBar.vue'
+import { useGameStore } from './stores/gameStore'
+
+const gameStore = useGameStore()
+
+onMounted(() => {
+  gameStore.initializeGame()
+})
 </script>
