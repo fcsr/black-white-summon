8f8bade8ae6a052c9a2c6d28e92602587b0da1e8
fcsr <gsfcsr@gmail.com>
2026-07-09 10:54:14 +0800

    Update TopBar.vue

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -4,10 +4,16 @@
       <div class="topbar__title">黑白召唤</div>
 
       <div class="topbar__resources">
-        <div class="resource-pill">金币 1000</div>
-        <div class="resource-pill">辉石 300</div>
-        <div class="resource-pill">体力 100</div>
+        <div class="resource-pill">金币 {{ gameStore.player.gold }}</div>
+        <div class="resource-pill">辉石 {{ gameStore.player.gem }}</div>
+        <div class="resource-pill">体力 {{ gameStore.player.energy }}</div>
       </div>
     </div>
   </header>
 </template>
+
+<script setup>
+import { useGameStore } from '../../stores/gameStore'
+
+const gameStore = useGameStore()
+</script>
