8bb956dab8c2a9d6c7974d1c1d4272f6b9b1faa6
fcsr <gsfcsr@gmail.com>
2026-07-09 10:02:42 +0800

    Create TopBar.vue

---
+++ src/components/layout/TopBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="topbar">
+    <div class="topbar__inner">
+      <div class="topbar__title">黑白召唤</div>
+
+      <div class="topbar__resources">
+        <div class="resource-pill">金币 1000</div>
+        <div class="resource-pill">辉石 300</div>
+        <div class="resource-pill">体力 100</div>
+      </div>
+    </div>
+  </header>
+</template>
